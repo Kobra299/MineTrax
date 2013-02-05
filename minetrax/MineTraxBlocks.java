56f7bb1c61b1ca217763a5a418a18e846dd0f288
Clank <brad.knez@gmail.com>
2013-02-04 19:41:57 -0500

    Made it simpler to add Items and Blocks
    Use MineTraxItems and MineTraxBlocks Now

---
+++ minetrax/MineTraxBlocks.java
@@ -0,0 +1,8 @@
+/* Read LICENSE.TXT for this mod's LICENSE! */
+package minetrax;
+public class MineTraxBlocks {
+// Add Blocks here
+	public static void init() {
+		// Add anythin else here, language registry, game registry etc.
+	}
+}
--- src/main/java/minetrax/MineTraxItems.java
+++ src/main/java/minetrax/MineTraxItems.java
@@ -4,9 +4,9 @@ package minetrax;
 import net.minecraft.item.Item;
 
 public class MineTraxItems {
-// Add Items here
 
     public static void init() {
         // Add anything else here, language registry, game registry etc.
     }
+
 }
