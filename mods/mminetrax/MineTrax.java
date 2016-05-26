ecf463f4f43e751378114e26df9fe97016609130
alexandrage <alexandrage@mail.ru>
2016-05-26 17:35:11 +0300

    Music Box added
    Music Box added

--- mods/mminetrax/MineTrax.java
+++ mods/mminetrax/MineTrax.java
@@ -8,11 +8,19 @@ import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.registry.GameData;
 import cpw.mods.fml.common.registry.GameRegistry;
 import cpw.mods.fml.common.registry.LanguageRegistry;
-import java.io.IOException;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import net.minecraft.block.Block;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemRecord;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.RegistryNamespaced;
 import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.common.config.Property;
@@ -32,37 +40,46 @@ public class MineTrax
   public static CommonProxyMineTrax proxyCommon = new CommonProxyMineTrax();
   public Configuration configFile;
   public static final RegistryNamespaced itemRegistry = GameData.getItemRegistry();
-
+  static List<Item> items = new ArrayList<Item>();
+  
   @Mod.EventHandler
-  public void preInit(FMLPreInitializationEvent event)
-    throws IOException
-  {
+  public void preInit(FMLPreInitializationEvent event) throws IOException {
     Configuration config = new Configuration(event.getSuggestedConfigurationFile());
     config.load();
     DataProxyMineTrax.discStartID = config.get("General", "DiscStartID", 15000).getInt(15000);
     this.numberOfDiscs = config.get("General", "NumberOfDiscs", 10).getInt();
 
     this.arrayDisc = new int[this.numberOfDiscs];
+    int i = -1;
     for (int curDisc = 0; curDisc < this.arrayDisc.length; curDisc++) {
+      i++;
       this.arrayDisc[curDisc] = curDisc;
       int discID = this.arrayDisc[curDisc] + DataProxyMineTrax.discStartID;
       String curDiscStr = Integer.toString(curDisc);
       this.songAuthor = config.get(curDiscStr, "SongAuthor", "None").getString();
       this.songTitle = config.get(curDiscStr, "SongTitle", "None").getString();
       this.songFilename = config.get(curDiscStr, "SongFilename", "None").getString(); 
-    newDisc = new ItemMineTraxRecord(this.songFilename, this.songAuthor, this.songTitle).setUnlocalizedName("MusicDisc" + curDisc).setMaxStackSize(1).setFull3D().setTextureName("mminetrax:disc");
-    itemRegistry.addObject(discID, "record"+curDisc, newDisc);
+      newDisc = new ItemMineTraxRecord(this.songFilename, this.songAuthor, this.songTitle).setUnlocalizedName("MusicDisc" + curDisc).setMaxStackSize(1).setFull3D().setTextureName("mminetrax:disc");
+      itemRegistry.addObject(discID, "record"+curDisc, newDisc);
       LanguageRegistry.addName(newDisc, "Music Disc");
+      items.add(newDisc);
     }
+    MusicBox.init();
+    GameRegistry.addRecipe(new ItemStack(MusicBox.Box), new Object[] {"###", "#I#", "###", '#', Items.iron_ingot, 'I', Items.flint});
 
     config.save();
   }
 
   @Mod.EventHandler
-  public void postInit(FMLPostInitializationEvent event)
-    throws IOException
-  {
+  public void postInit(FMLPostInitializationEvent event) throws IOException {
   }
+  public static CreativeTabs tabmusicdisc = new CreativeTabs("tabmusicdisc") {
+      @Override
+      @SideOnly(Side.CLIENT)
+      public Item getTabIconItem() {
+          return Items.record_13;
+      }
+  };
 
   public void parseMainConfig()
   {
---
+++ mods/mminetrax/MusicBox.java
@@ -0,0 +1,31 @@
+package mods.mminetrax;
+
+import java.util.Random;
+
+import cpw.mods.fml.common.registry.GameRegistry;
+import cpw.mods.fml.common.registry.LanguageRegistry;
+import net.minecraft.block.Block;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+
+public class MusicBox extends Item {
+	    public static Item Box;
+	    
+	    public static void init() {
+	    	Box = new MusicBox().setUnlocalizedName("MusicBox").setTextureName("mminetrax:disc").setCreativeTab(MineTrax.tabmusicdisc);
+	        GameRegistry.registerItem(Box, Box.getUnlocalizedName());
+	        LanguageRegistry.addName(Box, "Music Box");
+	    }
+	    
+	    @Override
+	    public ItemStack onItemRightClick(ItemStack itemstack, World world, EntityPlayer entityplayer) {
+	    	this.maxStackSize = 1;
+	    	world.playSoundAtEntity(entityplayer, "random.bow", 0.5F, 0.4F/(itemRand.nextFloat()*0.4F+0.8F));
+	    	Random ran = new Random();
+	    	int x = ran.nextInt(MineTrax.items.size()-1);
+	    	entityplayer.inventory.setInventorySlotContents(entityplayer.inventory.currentItem, new ItemStack(MineTrax.items.get(x), 1, 0));
+			return itemstack;
+	    }
+}
