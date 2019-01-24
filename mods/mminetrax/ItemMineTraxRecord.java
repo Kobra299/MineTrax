87f75d0bb83f53cabd73ad40fe16298c9ff00f63
alexandrage <alexandrage@mail.ru>
2019-01-24 19:23:38 +0300

    update 1.12.2
    update 1.12.2

--- mods/mminetrax/ItemMineTraxRecord.java
+++ mods/mminetrax/ItemMineTraxRecord.java
@@ -1,59 +1,34 @@
 package mods.mminetrax;
 
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.EnumRarity;
 import net.minecraft.item.ItemRecord;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
-import net.minecraft.util.StatCollector;
+import net.minecraft.util.SoundEvent;
+import net.minecraft.util.text.translation.I18n;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class ItemMineTraxRecord extends ItemRecord {
-	private static final Map recordMap = new HashMap();
-	public final String recordFileName;
 	public final String songAuthor;
 	public final String songTitle;
-
-	protected ItemMineTraxRecord(String recordFileName, String songAuthor, String songTitle) {
-		super(recordFileName);
-		this.recordFileName = recordFileName;
-		this.maxStackSize = 1;
+	protected ItemMineTraxRecord(String curDiscStr, String songAuthor, String songTitle, SoundEvent soundIn) {
+		super(songTitle, soundIn);
+		this.setUnlocalizedName(songTitle);
+		this.setRegistryName(curDiscStr+songTitle);
+		this.setCreativeTab(MineTrax.tabmusicdisc);
 		this.songAuthor = songAuthor;
 		this.songTitle = songTitle;
-		setCreativeTab(MineTrax.tabmusicdisc);
-		recordMap.put(recordFileName, this);
 	}
 
-	@SideOnly(Side.CLIENT)
-	@Override
-	public void addInformation(ItemStack par1ItemStack, EntityPlayer par2EntityPlayer, List par3List, boolean par4) {
-		par3List.add(getRecordNameLocal());
-	}
-
-	@SideOnly(Side.CLIENT)
-	@Override
-	public String getRecordNameLocal() {
-		return StatCollector.translateToLocal(this.songAuthor + " - " + this.songTitle);
-	}
-
-	public void func_94581_a(IIconRegister iconRegister) {
-		this.itemIcon = iconRegister.registerIcon("mminetrax:disc");
-	}
-
-	@SideOnly(Side.CLIENT)
-	@Override
-	public EnumRarity getRarity(ItemStack par1ItemStack) {
-		return EnumRarity.rare;
-	}
-
-	@Override
-	public ResourceLocation getRecordResource(String name) {
-		return new ResourceLocation("mminetrax:" + name);
-	}
+    @SideOnly(Side.CLIENT)
+    public String getRecordNameLocal()
+    {
+        return this.songAuthor + " - " + this.songTitle;
+    }
 }
--- mods/mminetrax/MineTrax.java
+++ mods/mminetrax/MineTrax.java
@@ -1,75 +1,74 @@
 package mods.mminetrax;
 
-import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.Mod.EventHandler;
-import cpw.mods.fml.common.SidedProxy;
-import cpw.mods.fml.common.event.FMLPostInitializationEvent;
-import cpw.mods.fml.common.event.FMLPreInitializationEvent;
-import cpw.mods.fml.common.registry.GameData;
-import cpw.mods.fml.common.registry.GameRegistry;
-import cpw.mods.fml.common.registry.LanguageRegistry;
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import net.minecraft.block.Block;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.init.Items;
+import net.minecraft.init.SoundEvents;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemRecord;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.RegistryNamespaced;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.registry.RegistryNamespaced;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.common.config.Property;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.fml.common.FMLCommonHandler;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.registries.GameData;
 
-@Mod(modid="mMineTrax", name="MineTrax", version="1.1.4")
+@Mod(modid = "mminetrax", name = "MineTrax", version = "1.12.2", acceptedMinecraftVersions = "1.12.2")
 public class MineTrax {
 	int[] arrayDisc;
 	public int numberOfDiscs;
-	public static Item newDisc;
 	public String songAuthor;
 	public String songTitle;
 	public String songFilename;
-  
-
-	@SidedProxy(clientSide="mods.mminetrax.ClientProxyMineTrax", serverSide="mods.mminetrax.CommonProxyMineTrax")
-	public static ClientProxyMineTrax proxyCommon = new ClientProxyMineTrax();
 	public Configuration configFile;
-	public static final RegistryNamespaced itemRegistry = GameData.getItemRegistry();
-	static List<Item> items = new ArrayList<Item>();
-  
+	public static List<Item> items = new ArrayList<Item>();
+	public static MusicBox box;
+
 	@EventHandler
 	public void preInit(FMLPreInitializationEvent event) throws IOException {
+		box = new MusicBox();
 		Configuration config = new Configuration(event.getSuggestedConfigurationFile());
 		config.load();
-		DataProxyMineTrax.discStartID = config.get("General", "DiscStartID", 15000).getInt(15000);
 		this.numberOfDiscs = config.get("General", "NumberOfDiscs", 10).getInt();
 		this.arrayDisc = new int[this.numberOfDiscs];
 		int i = -1;
 		for (int curDisc = 0; curDisc < this.arrayDisc.length; curDisc++) {
 			i++;
 			this.arrayDisc[curDisc] = curDisc;
-			int discID = this.arrayDisc[curDisc] + DataProxyMineTrax.discStartID;
 			String curDiscStr = Integer.toString(curDisc);
-			this.songAuthor = config.get(curDiscStr, "SongAuthor", "None").getString();
-			this.songTitle = config.get(curDiscStr, "SongTitle", "None").getString();
-			this.songFilename = config.get(curDiscStr, "SongFilename", "None").getString(); 
-			newDisc = new ItemMineTraxRecord(this.songFilename, this.songAuthor, this.songTitle).setUnlocalizedName("MusicDisc" + curDisc).setMaxStackSize(1).setFull3D().setTextureName("mminetrax:disc");
-			itemRegistry.addObject(discID, "record"+curDisc, newDisc);
-			LanguageRegistry.addName(newDisc, "Music Disc");
-			items.add(newDisc);
+			this.songAuthor = config.get(curDiscStr, "SongAuthor", "none").getString();
+			this.songTitle = config.get(curDiscStr, "SongTitle", "none").getString();
+			this.songFilename = config.get(curDiscStr, "SongFilename", "none").getString();
+			items.add(new ItemMineTraxRecord(curDiscStr, songAuthor, songTitle, new Sound(new ResourceLocation("mminetrax:records."+this.songFilename))));
 		}
-		MusicBox.init();
-		GameRegistry.addRecipe(new ItemStack(MusicBox.Box), new Object[] {"###", "#I#", "###", '#', Items.iron_ingot, 'I', Items.flint});
 		config.save();
+	}
+	
+	@EventHandler
+	public void init(FMLInitializationEvent event) throws IOException {
+		MinecraftForge.EVENT_BUS.register(this);
 	}
 
 	public static CreativeTabs tabmusicdisc = new CreativeTabs("tabmusicdisc") {
 		@Override
 		@SideOnly(Side.CLIENT)
-		public Item getTabIconItem() {
-			return Items.record_13;
+		public ItemStack getTabIconItem() {
+			return new ItemStack(Items.RECORD_13);
 		}
 	};
 }
